9027c5a3e41a4c751786c8845f9340313175b2f6
Siyad Savad <siyadsavad313@gmail.com>
2025-10-05 13:22:26 +0530

    models added for Product and order

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+
+  def __str__(self):
+    return self.name
+  
+
+class Order(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.PositiveIntegerField()
+  total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+  payment_id = models.CharField(max_length=100, unique=True)
+  paid = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=False)
+
+
