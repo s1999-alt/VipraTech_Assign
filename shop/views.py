2d9c4c3b4c647302f172aa8669ff3cd4c5392156
Siyad Savad <siyadsavad313@gmail.com>
2025-10-05 13:47:45 +0530

    write view for index/home page + index.html page is added inside the shop app

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product, Order
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+
+def index(request):
+  products = Product.objects.all()
+  orders = Order.objects.filter(paid=True)
+
+  context = {
+    "products": products,
+    "orders": orders,
+    "stripe_public_key": settings.STRIPE_PUBLIC_KEY
+  }
+  return render(request, "index.html", context)
+
