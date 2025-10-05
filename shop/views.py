e328dc1db05b48ad777d826f4c1095f166accc1d
Siyad Savad <siyadsavad313@gmail.com>
2025-10-06 00:13:39 +0530

    signup, login , logout views writed

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Product, Order
 from django.conf import settings
@@ -89,6 +92,40 @@ def success_view(request):
 
     # Redirect to home
     return redirect("/")
+
+
+def signup_view(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('/')
+    
+  else:
+     form = UserCreationForm()
+  return render(request, 'signup.html', {'form': form})
+
+
+
+def login_view(request):
+  if request.method == "POST":
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('/')
+  
+  else:
+     form = AuthenticationForm()
+  return render(request, 'login.html', {'form': form})
+
+
+ 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
 
 
 
