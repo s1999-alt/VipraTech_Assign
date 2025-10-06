cdf4580ae599e1aec13b4e80104d50e35f242407
Siyad Savad <siyadsavad313@gmail.com>
2025-10-06 11:38:40 +0530

    images added in the product model and display in the frontend

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
   name = models.CharField(max_length=100)
   price = models.DecimalField(max_digits=10, decimal_places=2)
+  image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
   def __str__(self):
     return self.name
